aeacc82b293b86406d9a63a8d7fe4f80feeab83f
NapalmZ <willy.baessato@gmail.com>
2026-01-13 15:37:15 +0100

    Creato un endpoint apposito per healthcheck invece di usare "version"

--- backend/routes/system.py
+++ backend/routes/system.py
@@ -12,8 +12,19 @@ router = APIRouter()
 class VersionResponse(BaseModel):
     version: str
 
+##################################################
+# Modello per la risposta all'HEALTHCHECK dell'API
+class HealthCheckResponse(BaseModel):
+    version: str
+
 #################################################
 # Endpoint per ottenere la versione dell'API
 @router.get("/version", response_model=VersionResponse)
 def get_api_version():
     return {"version": os.getenv("API_VERSION", "unknown")}
+
+#################################################
+# Endpoint usato dall'HEALTHCHECK di Docker
+@router.get("/health-check", response_model=HealthCheckResponse)
+def get_health():
+    return {"status": "Everything is OK", "version": os.getenv("API_VERSION", "unknown")}
